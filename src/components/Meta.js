9b12232a85937098e5df0091672b34daa5cdfbd0
Clark Sell <clark.sell@csell.net>
2019-02-19 08:38:52 -0700

    changing some copy around

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -9,33 +9,21 @@ const Meta = () => (
 
     <title>THAT Conference - Slack Signup</title>
 
-    <meta
-      name="description"
-      content="Summer Camp For Geeks"
-    />
-    <meta
-      name="keywords"
-      content="TIKI Bar, Resturant, Twin Lakes, Lake Elizabeth"
-    />
+    <meta name="description" content="Summer Camp For Geeks" />
+    <meta name="keywords" content="THAT Conference, Tech Conference" />
 
     <meta property="og:type" content="website" />
     <meta property="og:title" content="THAT Conference" />
-    <meta
-      property="og:description"
-      content="A Tropical Oasis in the least likely of places!"
-    />
-    <meta property="og:url" content="https://sandbarandislandgrill.com/" />
-    <meta property="og:site_name" content="Sand Bar And Island Grill" />
+    <meta property="og:description" content="Summer Camp for Geeks!" />
+    <meta property="og:url" content="https://slack.thatconference.com/" />
+    <meta property="og:site_name" content="THAT Conference" />
     <meta property="og:image" content="https://s0.wp.com/i/blank.jpg" />
     <meta property="og:locale" content="en_US" />
 
-    <meta name="twitter:site" content="@SandBar" />
+    <meta name="twitter:site" content="@thatconference" />
     <meta name="twitter:title" content="FILL ME OUT" />
     <meta name="twitter:card" content="summary" />
-    <meta
-      name="twitter:description"
-      content="A Tropical Oasis in the least likely of places!"
-    />
+    <meta name="twitter:description" content="Summer Camp for Geeks!" />
     <meta
       name="twitter:image"
       content="https://www.thatconference.com/images/icons/opengraph.jpg"
