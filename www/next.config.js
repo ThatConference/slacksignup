32a4cd6eeee47904010e70d565d6829fef11ace8
Clark Sell <clark.sell@csell.net>
2019-02-23 17:09:40 -0600

    removes old config info

--- www/next.config.js
+++ www/next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  target: 'serverless',
-  env: {
-    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY // Pass through env variables
-    // reCaptchaSiteKey: '6Ld2d5MUAAAAAHXiYgZGeGKgvidYlTW3zQ11QPQA' // Pass through env variables
-  }
+  target: 'serverless'
 };
