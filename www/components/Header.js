33a64557f6168ab84a79aeab8f0ffcfb2886bea2
Rachael Andrew <r.andrew.dev@gmail.com>
2020-10-29 19:48:37 -0500

    changed THAT logo to white THAT.svg, centered

--- www/components/Header.js
+++ www/components/Header.js
@@ -1,26 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const SVG = ({
-//   style = {},
-//   fill = '#fff',
-//   width = '100%',
-//   className = 'header-logo',
-//   height = '100%',
-//   viewBox = '0 0 32 32',
-// }) => 
-//   <svg
-//     width={width}
-//     style={style}
-//     height={height}
-//     viewBox={viewBox}
-//     className={className}
-//     xmlns="http://www.w3.org/2000/svg"
-//     xmlnsXlink="http://www.w3.org/1999/xlink"
-//   >
-//       <path d="../static/images/THAT.svg" fill={fill} />
-//   </svg>;
-
 
 const Header = ({ className }) => (
   <header className={className}>
