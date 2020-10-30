fcca8459527e7fc1f2f95cfa9d0921f747ff5840
Rachael Andrew <r.andrew.dev@gmail.com>
2020-10-29 20:27:27 -0500

    Update colors and brand (#32)
    * updated color scheme and branding to match 2020 edition of that.us and thatconference.com
    
    * changed Logo to white THAT.svg, centered
    
    * changed THAT logo to white THAT.svg, centered
    
    Co-authored-by: Clark Sell <clark.sell@csell.net>

--- www/components/Header.js
+++ www/components/Header.js
@@ -1,21 +1,19 @@
+import React from 'react';
 import styled from 'styled-components';
+
 
 const Header = ({ className }) => (
   <header className={className}>
     <div className="header-container">
       <a href="//www.thatconference.com">
-        <img
-          className="header-logo"
-          src="/static/images/TC-Head-Logo-Red.png"
-          alt="THAT Conference"
-        />
+        <img src='../static/images/THAT.svg' alt="THAT Logo" class="that-logo"/>
       </a>
     </div>
   </header>
 );
 
 export default styled(Header)`
-  height: 7.2rem;
+  height: 118px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -27,10 +25,22 @@ export default styled(Header)`
     padding: 0 1.5rem;
     width: 100%;
     max-width: 1170px;
+    text-align: center;
   }
 
   .header-logo {
     position: absolute;
     top: 0;
   }
+
+  img.that-logo {
+    filter: brightness(0) saturate(100%) invert(99%) sepia(2%) saturate(315%) hue-rotate(170deg) brightness(117%) contrast(100%);
+    padding-top: 1%;
+    width: 50%;
+  }
+
+header-logo svg text {
+  font-size: 42px;
+}
+
 `;
